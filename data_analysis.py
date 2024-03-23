00f40f1a757f7521895a1a51b7c410f25b25e622
chrisharpe <4chrisharpe@gmail.com>
2024-03-23 09:30:10 -0500

    adjusts activity instance classification, data visualizations

--- data_analysis.py
+++ data_analysis.py
@@ -1,28 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def load_data():
     hourly_intensities = pd.read_csv('hourlyIntensities_merged.csv')
     hourly_intensities['ActivityHour'] = pd.to_datetime(
-        hourly_intensities['ActivityHour'], errors='coerce')
+        hourly_intensities['ActivityHour'], format="%m/%d/%Y %I:%M:%S %p", errors='coerce')
 
     hourly_steps = pd.read_csv('hourlySteps_merged.csv')
     hourly_steps['ActivityHour'] = pd.to_datetime(
-        hourly_steps['ActivityHour'], errors='coerce')
+        hourly_steps['ActivityHour'], format="%m/%d/%Y %I:%M:%S %p", errors='coerce')
 
     daily_activity = pd.read_csv('dailyActivity_merged.csv')
     daily_activity['ActivityDate'] = pd.to_datetime(
-        daily_activity['ActivityDate'], errors='coerce')
+        daily_activity['ActivityDate'], format="%m/%d/%y", errors='coerce')
 
     return hourly_intensities, hourly_steps, daily_activity
 
 
 def prepare_data(hourly_intensities, hourly_steps, daily_activity):
-    # Calculate median steps for running identification
-    median_steps = hourly_steps.groupby(
-        'Id')['StepTotal'].median().reset_index(name='MedianStepCount')
-
     # Merge hourly datasets for a comprehensive view
     hourly_data = pd.merge(hourly_steps, hourly_intensities[[
                            'Id', 'ActivityHour', 'TotalIntensity']], on=['Id', 'ActivityHour'])
@@ -30,76 +27,121 @@ def prepare_data(hourly_intensities, hourly_steps, daily_activity):
     # Ensure the 'Date' column for merging is just the date part, derived from 'ActivityHour'
     hourly_data['Date'] = hourly_data['ActivityHour'].dt.date
 
+    # Remove entries with zero steps
+    daily_activity = daily_activity[daily_activity['TotalSteps'] > 100]
+
     # Calculate ActiveDistance for cycling identification
-    daily_activity['TotalActiveDistance'] = daily_activity['VeryActiveDistance'] + \
+    daily_activity.loc[:, 'TotalActiveDistance'] = daily_activity['VeryActiveDistance'] + \
         daily_activity['ModeratelyActiveDistance']
 
-    # Convert 'ActivityDate' to just the date part for consistency
+    # Convert 'ActivityDate' to date
     daily_activity['ActivityDate'] = daily_activity['ActivityDate'].dt.date
 
-    return median_steps, hourly_data, daily_activity
+    # Calculate the AMStepRatio
+    daily_activity.loc[:, 'AMStepRatio'] = (
+        daily_activity['VeryActiveMinutes'] + daily_activity['FairlyActiveMinutes']) / daily_activity['TotalSteps']
+
+    # Handle potential division by zero issues by replacing inf with NaN
+    daily_activity.loc[:, 'AMStepRatio'] = daily_activity['AMStepRatio'].replace([
+                                                                                 np.inf, -np.inf], np.nan)
+
+    # Drop rows where AMStepRatio is NaN (cases where TotalSteps is zero)
+    daily_activity.dropna(subset=['AMStepRatio'], inplace=True)
+
+    return hourly_data, daily_activity
 
 
-def identify_activities(median_steps, hourly_data, daily_activity):
+def identify_activities(hourly_data, daily_activity):
     # Prepare the hourly_data DataFrame by merging necessary data
-    hourly_data = pd.merge(hourly_data, median_steps, on='Id')
-    hourly_data = pd.merge(hourly_data, daily_activity[['Id', 'ActivityDate', 'TotalActiveDistance']], left_on=[
+    merged_data = pd.merge(hourly_data, daily_activity[['Id', 'ActivityDate', 'TotalActiveDistance', 'AMStepRatio']], left_on=[
                            'Id', 'Date'], right_on=['Id', 'ActivityDate'])
     # Ensure data is sorted
-    hourly_data.sort_values(by=['Id', 'ActivityHour'], inplace=True)
+    merged_data.sort_values(by=['Id', 'ActivityHour'], inplace=True)
 
-    # Running
-    hourly_data['IsRunning'] = (hourly_data['TotalActiveDistance'] > 3.2) & (
-        hourly_data['StepTotal'] > hourly_data['MedianStepCount'])
+# Running
+    merged_data['IsRunning'] = (merged_data['TotalActiveDistance'] > 3.2) & \
+                               (merged_data['TotalIntensity'] > 40) & \
+                               (merged_data['StepTotal'] > 2500)
 
     # Mark start of new running instances (when previous row is not running, but current is)
-    hourly_data['PrevIsRunning'] = hourly_data.groupby('Id')[
+    merged_data['PrevIsRunning'] = merged_data.groupby('Id')[
         'IsRunning'].shift(1)
-    hourly_data['StartNewRun'] = (hourly_data['IsRunning']) & (
-        hourly_data['PrevIsRunning'] != True)
+    merged_data['StartNewRun'] = (merged_data['IsRunning']) & \
+                                 (merged_data['PrevIsRunning'] != True)
 
     # Assign an instance ID to each running block
-    hourly_data['RunningInstance'] = hourly_data.groupby('Id')[
+    merged_data['RunningInstance'] = merged_data.groupby('Id')[
         'StartNewRun'].cumsum()
 
-    # Cycling
-    daily_activity['IsCyclingDay'] = (daily_activity['TotalActiveDistance'] > 20) & \
-                                     ((daily_activity['TotalSteps'] / daily_activity['TotalActiveDistance']) <= (
-                                         2050 / 1.6))
-    hourly_data['IsCycling'] = hourly_data['Date'].isin(
-        daily_activity[daily_activity['IsCyclingDay']]['ActivityDate'])
 
-    # Weightlifting
-    hourly_data['IsWeightlifting'] = (hourly_data['TotalIntensity'] > 50) & (
-        hourly_data['StepTotal'] < 2000)
+# Cycling
+    # Calculate the user-specific 75th percentile of AMStepRatio
+    user_specific_75th = daily_activity.groupby('Id')['AMStepRatio'].quantile(
+        0.75).reset_index(name='User75thPercentile')
 
-    # Collapse hourly running data into a summary of instances
-    hourly_data['IsRunningInstance'] = hourly_data['StartNewRun'] & hourly_data['IsRunning']
-    running_instances_summary = hourly_data.groupby(
+    # Merge this user-specific percentile back into the daily_activity dataframe
+    daily_activity = pd.merge(daily_activity, user_specific_75th, on='Id')
+
+    # Calculate ActiveDistance for each day
+    daily_activity['ActiveDistance'] = daily_activity['VeryActiveDistance'] + \
+        daily_activity['ModeratelyActiveDistance'] + \
+        daily_activity['LightActiveDistance']
+
+    # Refine cycling detection with the ActiveDistance criteria and exclude days with zero steps
+    daily_activity['IsCyclingDay'] = (daily_activity['AMStepRatio'] > daily_activity['User75thPercentile']) & (
+        daily_activity['TotalActiveDistance'] > 20)
+
+# Weightlifting
+    merged_data['IsWeightlifting'] = (merged_data['TotalIntensity'] > 50) & (
+        merged_data['StepTotal'] < 2000)
+
+    # Mark start of new weightlifting sessions
+    merged_data['PrevIsWeightlifting'] = merged_data.groupby(
+        'Id')['IsWeightlifting'].shift(1)
+    merged_data['StartNewWeightliftingSession'] = (
+        merged_data['IsWeightlifting']) & (merged_data['PrevIsWeightlifting'] != True)
+
+    # Assign an instance ID to each weightlifting block
+    merged_data['WeightliftingInstance'] = merged_data.groupby(
+        'Id')['StartNewWeightliftingSession'].cumsum()
+
+    # Collapse data into a summary of instances
+    merged_data['IsRunningInstance'] = merged_data['StartNewRun'] & merged_data['IsRunning']
+    running_instances_summary = merged_data.groupby(
         'Id')['IsRunningInstance'].sum().reset_index(name='RunningInstances')
-    cycling_instances_summary = hourly_data.groupby(
-        'Id')['IsCycling'].sum().reset_index(name='CyclingInstances')
-    weightlifting_instances_summary = hourly_data.groupby(
-        'Id')['IsWeightlifting'].sum().reset_index(name='WeightliftingInstances')
-
+    cycling_instances_summary = daily_activity.groupby(
+        'Id')['IsCyclingDay'].sum().reset_index(name='CyclingInstances')
+    weightlifting_instances_summary = merged_data[merged_data['IsWeightlifting']].groupby(
+        'Id')['WeightliftingInstance'].nunique().reset_index(name='WeightliftingInstances')
     # Merge summaries for visualization
-    summary = running_instances_summary
-    summary = summary.merge(cycling_instances_summary, on='Id')
-    summary = summary.merge(weightlifting_instances_summary, on='Id')
+    summary = pd.merge(cycling_instances_summary,
+                       running_instances_summary, on='Id', how='outer')
+    summary = pd.merge(
+        summary, weightlifting_instances_summary, on='Id', how='outer')
+
+    # Replace NaNs with 0 for activities without instances
+    summary.fillna(0, inplace=True)
 
     return summary
 
 
 def predict_activities():
     hourly_intensities, hourly_steps, daily_activity = load_data()
-    median_steps, hourly_data, daily_activity_prepared = prepare_data(
+    hourly_prepared, daily_prepared = prepare_data(
         hourly_intensities, hourly_steps, daily_activity)
     activity_instances = identify_activities(
-        median_steps, hourly_data, daily_activity_prepared)
+        hourly_prepared, daily_prepared)
     return activity_instances
 
 
 def visualize_activities(activity_instances):
+    # Define colors for each activity
+    activity_colors = {
+        'Running': '#57A26E',
+        'Cycling': '#E47979',
+        'Weightlifting': '#0097B2',
+    }
+
     # Pie chart
     total_running = activity_instances['RunningInstances'].sum()
     total_cycling = activity_instances['CyclingInstances'].sum()
@@ -111,19 +153,32 @@ def visualize_activities(activity_instances):
         'Weightlifting': total_weightlifting,
     }
 
+    # Extract colors for the pie chart based on the activity keys
+    pie_colors = [activity_colors[activity]
+                  for activity in activity_counts.keys()]
+
     plt.figure(figsize=(8, 8))
-    plt.pie(activity_counts.values(), labels=activity_counts.keys(),
+    plt.pie(activity_counts.values(), labels=activity_counts.keys(), colors=pie_colors,
             autopct='%1.1f%%', startangle=140)
     plt.title('Activity Instances Comparison')
     plt.show()
 
-    # Bar graph for each user's activity instances
-    activity_instances.set_index('Id').plot(
-        kind='bar', stacked=True, figsize=(14, 8))
+   # Bar graph for each user's activity instances
+    activity_df = activity_instances.set_index('Id')
+    # Use the predefined colors for consistency
+    bar_colors = [activity_colors['Cycling'],
+                  activity_colors['Running'], activity_colors['Weightlifting']]
+
+    ax = activity_df[['CyclingInstances', 'RunningInstances', 'WeightliftingInstances']].plot(
+        kind='bar', stacked=True, figsize=(14, 8), color=bar_colors)
     plt.title('Number of Activity Instances per User')
     plt.ylabel('Number of Instances')
     plt.xticks(rotation=45)
-    plt.legend(['Running Instances', 'Cycling Instances',
+
+    # Correcting the alignment of x-axis labels
+    ax.set_xticklabels(activity_df.index, rotation=45, ha="right")
+
+    plt.legend(['Cycling Instances', 'Running Instances',
                'Weightlifting Instances'])
     plt.tight_layout()
     plt.show()
@@ -132,5 +187,3 @@ def visualize_activities(activity_instances):
 if __name__ == "__main__":
     activity_instances = predict_activities()
     visualize_activities(activity_instances)
-    # print(activity_instances[['ActivityHour', 'IsRunning',
-    #       'IsCycling', 'IsWeightlifting']].head())
